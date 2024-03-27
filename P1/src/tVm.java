3ec73991e62c9b2337d9e98f412ea8c76d581839
AlexGoodlife <novaisboavida@gmail.com>
2024-03-27 17:08:54 +0000

    oopsie :)

--- P1/src/tVm.java
+++ P1/src/tVm.java
@@ -171,10 +171,10 @@ public class tVm {
         Object right = this.executionStack.pop();
         Object left = this.executionStack.pop();
         switch (instruction.getInstruction()) {
-            case IADD, ISUB, IMOD, IMULT, IEQ, INEQ, ILEQ, ILT -> intOperation(right, left, instruction.getInstruction());
-            case DADD, DSUB, DMULT, DEQ, DNEQ, DLEQ, DLT -> doubleOperation(right, left, instruction.getInstruction());
-            case SNEQ,SADD,SEQ -> stringOperation(right, left, instruction.getInstruction());
-            case BEQ,BNEQ -> booleanOperation(right, left, instruction.getInstruction());
+            case IADD, ISUB, IMOD, IMULT, IEQ, INEQ, ILEQ, ILT -> intOperation(left, right, instruction.getInstruction());
+            case DADD, DSUB, DMULT, DEQ, DNEQ, DLEQ, DLT -> doubleOperation(left, right, instruction.getInstruction());
+            case SNEQ,SADD,SEQ -> stringOperation(left, right, instruction.getInstruction());
+            case BEQ,BNEQ -> booleanOperation(left, right, instruction.getInstruction());
         }
     }
 
